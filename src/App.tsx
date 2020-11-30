a603e98100398aaa5972f45d7c6b8a0e56fcdff9
Maksim Gachevskiy <hektodaleky@gmail.com>
2020-11-30 13:50:03 +0600

    added console

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import OrderPage from './containers/OrderPage';
 import './App.scss';
+import Cookies from "js-cookie";
+
 
 function App() {
+    console.log('TAXI', Cookies.get('auth_token'));
+    console.log('TAXI',Cookies.get('refresh_token'))
   return (
       <Router>
         <Switch>

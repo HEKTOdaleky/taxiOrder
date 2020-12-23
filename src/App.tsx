356e1c38e6041e85d30d8ac395b51f721ea2fabf
Maksim Gachevskiy <hektodaleky@gmail.com>
2020-12-23 20:42:15 +0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import OrderPage from './containers/OrderPage';
 import './App.scss';
-import Cookies from "js-cookie";
 
 
 function App() {
